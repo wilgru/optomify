588e6ff4b453633a4afc55187551c7956a13d19a
wilgru <william.gruszka@gmail.com>
2022-07-20 21:26:58 +1000

    added Prescription subdoc

--- server/models/ClinicalFile.js
+++ server/models/ClinicalFile.js
@@ -1,38 +1,37 @@
 const { Schema, model } = require('mongoose');
+const PrescriptionSchema = require('./PrescriptionSchema');
 
 ["precription", "health check", "report"];
 
 const clinicalFile = new Schema({
-    file_type: {
-      type: String,
-      required: true
-    },
-    patient: {
+  file_type: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  date_created: {
+    type: Date,
+    required: true,
+    default: () => (Date.now())
+  },
+  text_field: {
+    type: String,
+    required: true
+  },
+  medicare_item: {
+    type: String,
+    required: true
+  },
+  recall: {
+    type: Date
+  },
+  prev_prescription: PrescriptionSchema,
+  given_prescription: PrescriptionSchema,
+});
 
-    },
-    date_created: {
-      type: Date,
-      required: true
-    },
-    text_field: {
-      type: String,
-      required: true
-    },
-    medicare_item: {
-      type: String,
-      required: true
-    },
-    recall: {
-      type: Date
-    },
-    prev_prescription: { // use prescriptionSchema
-      required: true
-    },
-    given_prescription: { // use prescriptionSchema
-      required: true
-    },
-  });
-  
-  const ClinicalFile = model('ClinicalFile', clinicalFile);
-  
-  module.exports = ClinicalFile;
+const ClinicalFile = model('ClinicalFile', clinicalFile);
+
+module.exports = ClinicalFile;
