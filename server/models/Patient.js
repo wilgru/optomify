fcdf847e9d4bc8a0d5d60ec68b2739832858e531
wilgru <william.gruszka@gmail.com>
2022-07-20 21:36:32 +1000

    added clinical_files

--- server/models/Patient.js
+++ server/models/Patient.js
@@ -1,41 +1,44 @@
 const { Schema, model } = require('mongoose');
+const ClinicalFile = require('./ClinicalFile');
+const Note = require('./Note');
 
 const patient = new Schema({
-    first_name: {
-      type: String,
-      required: true
-    },
-    last_name: {
-      type: String,
-      required: true
-    },
-    dob: {
-      type: Date,
-      required: true
-    },
-    mobile_number: {
-      type: String,
-      required: true
-    },
-    has_medicare: {
-      type: Boolean,
-      required: true
-    },
-    medicare_number: {
-      type: String
-    },
-    medicare_ref: {
-      type: String
-    },
-    medicare_exp: {
-      type: Date
-    },
-    clinical_files: {
-    },
-    misc_notes: {
-    }
-  });
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: Date,
+    required: true
+  },
+  mobile_number: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  has_medicare: {
+    type: Boolean,
+    required: true
+  },
+  medicare_number: {
+    type: String
+  },
+  medicare_ref: {
+    type: String
+  },
+  medicare_exp: {
+    type: Date
+  },
+  clinical_files: [ClinicalFile],
+  notes: [Note]
+});
   
-  const Patient = model('Patient', patient);
-  
-  module.exports = Patient;
+const Patient = model('Patient', patient);
+
+module.exports = Patient;
