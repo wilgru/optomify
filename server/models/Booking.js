661e8d38fa408718f664da119f79fe5a32cbfbd3
wilgru <william.gruszka@gmail.com>
2022-07-20 21:26:41 +1000

    added Patient subdoc

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,27 +1,29 @@
 const { Schema, model } = require('mongoose');
+const Patient = require('./Patient');
 
 const bookingSchema = new Schema({
-    date: {
-      type: Date,
-      required: true,
-    },
-    booking_start: {
-      type: Date,
-      required: true,
-    },
-    booking_end: {
-      type: Date,
-      required: true,
-    },
-    patient: {
-      required: true,
-    },
-    booking_type: {
-      type: String,
-      required: true,
-    }
-  });
-  
-  const Booking = model('Booking', bookingSchema);
-  
-  module.exports = Booking;
+  date: {
+    type: Date,
+    required: true,
+  },
+  booking_start: {
+    type: Date,
+    required: true,
+  },
+  booking_end: {
+    type: Date,
+    required: true,
+  },
+  patient: Patient,
+  booking_note: {
+    type: String
+  },
+  booking_type: {
+    type: String,
+    required: true,
+  }
+});
+
+const Booking = model('Booking', bookingSchema);
+
+module.exports = Booking;
