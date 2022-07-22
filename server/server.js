4f23a1ef10b2edc00b002415c54f0ae9ef266133
wilgru <william.gruszka@gmail.com>
2022-07-23 08:00:26 +1000

    added Auth context middleware to ApolloServer

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
+const { AuthMiddleware } = require('./utils/Auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,9 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    AuthMiddleware
+  }
 });
 
 // middlewares
