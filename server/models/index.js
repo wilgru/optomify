4cabebe73a03c1c2be821ad1062d5cce41a17c9e
wilgru <william.gruszka@gmail.com>
2022-07-22 22:08:09 +1000

    exporting User

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,8 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const Booking = require('./Booking');
+const BookSetup = require('./BookSetup');
+const ClinicalFile = require('./ClinicalFile');
+const Note = require('./Note');
+const Patient = require('./Patient');
+const User = require('./User');
 
-module.exports = { Tech, Matchup };
+module.exports = { Booking, BookSetup, ClinicalFile, Note, Patient, User };
