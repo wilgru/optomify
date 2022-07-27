82ecb2d2bd3d5e74bac6f709f68cdb8e8e83cedf
wilgru <william.gruszka@gmail.com>
2022-07-27 23:41:17 +1000

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,105 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+// react router
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Ant Design
+// import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+
+// components
+import Navbar from './components/Navbar';
+
+// Pages
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+
+// Apollo imports
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Construct the main GraphQL API endpoint to /graphql 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Setting up Apollo Client
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// Antd Layout components
+const { Footer } = Layout;
+
+// App
+const App = () => (
+  <ApolloProvider client={client}>
+     <Router>
+      <Layout>
+        <Navbar />
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/bookings' element={<Bookings />}/>
+        </Routes>
+        <Footer style={{textAlign: 'center'}}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+     </Router>
+  </ApolloProvider>
+);
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <Router>
+//         <>
+//           <Navbar />
+//           <Routes>
+//             <Route 
+//               path='/dashboard'
+//               element={<Dashboard />}
+//             />
+//             <Route 
+//               path='/bookings'
+//               element={<Bookings />}
+//             />
+//             <Route 
+//               path='*'
+//               element={<h1 className='display-2'>Wrong page!</h1>}
+//             />
+//           </Routes>
+//         </>
+//       </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
