d591734ace936560318190a6a7ae872619e33838
wilgru <william.gruszka@gmail.com>
2022-07-27 23:40:52 +1000

    added debugging code

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,32 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+// import React, { useState } from 'react';
+// import { render } from 'react-dom';
+// import { DatePicker, message } from 'antd';
+// import 'antd/dist/antd.css';
+// import './index.css';
+
+// const App = () => {
+//   const [date, setDate] = useState(null);
+//   const handleChange = value => {
+//     message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+//     setDate(value);
+//   };
+//   return (
+//     <div style={{ width: 400, margin: '100px auto' }}>
+//       <DatePicker onChange={handleChange} />
+//       <div style={{ marginTop: 16 }}>
+//         Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+//       </div>
+//     </div>
+//   );
+// };
+
+// render(<App />, document.getElementById('root'));
