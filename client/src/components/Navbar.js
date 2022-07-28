ab7686c8bb4b31d4fe406a6b89c4c14cd85f9e4f
wilgru <william.gruszka@gmail.com>
2022-07-28 16:00:22 +1000

    new titles for nav buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,24 +19,35 @@ const items1old = ['1', '2', '3'].map((key) => ({
 const items1 = [
     {
         key: 1,
-        label: `Dashboard`,
+        label: (            
+            <Link to="/dashboard">
+                Dashboard
+            </Link>
+        ),
     },
     {
         key: 2,
-        label: `Bookings`,
+        label: (            
+            <Link to="/bookings">
+                Bookings
+            </Link>
+        ),
     },
     {
         key: 3,
-        label: `Patients`,
+        label: (            
+            <Link to="/patients">
+                Patients
+            </Link>
+        ),
     },
 ]
-  
 
 const AppNavbar = () => {
   return (
     <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+        <Menu mode="horizontal" defaultSelectedKeys={['2']} items={items1}/>
     </Header>
   );
 };
