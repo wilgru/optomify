6ed3b4218b2b6f0f977f89ceda5f322f72b93cca
wilgru <william.gruszka@gmail.com>
2022-07-29 08:56:25 +1000

    using login mutation

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,42 +2,38 @@ import { Button, Checkbox, Form, Input, DatePicker } from 'antd';
 import React, { useEffect, useState } from 'react';
 
 // grpahQL
-import { CREATE_NEW_PATIENT_AND_BOOKING } from '../graphql/mutations';
+import { LOGIN } from '../graphql/mutations';
 import { GET_BOOK_SETUPS } from '../graphql/queries';
 import { useMutation } from '@apollo/client';
+import auth from '../utils/auth';
 
 const Login = (props) => {
     const [form] = Form.useForm();
 
-    const [createNewPatientAndBooking, { data, loading, error }] = useMutation(CREATE_NEW_PATIENT_AND_BOOKING);
+    const [loginMutation, { error }] = useMutation(LOGIN);
 
     // const [hasMedicare, setHasMedicare] = useState(true);
 
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         console.log('Success:', values);
 
-        createNewPatientAndBooking({
-            variables: {
-                firstName: values.first_name,
-                lastName: values.last_name,
-                dob: values.dob,
-                email: values.email,
-                mobileNumber: values.mobile_number,
-                hasMedicare: values.has_medicare,
-                medicareNumber: values.medicare_number,
-                medicare_Ref: values.medicare_ref,
-                medicare_Exp: values.medicare_exp,
-                bookingDate: props.bookingDate,
-                bookingStart: props.bookingStart,
-                bookingEnd: props.bookingEnd,
-                bookingType: "General eye test"
-            },
-            refetchQueries: [
-                {query: GET_BOOK_SETUPS},
-                GET_BOOK_SETUPS
-            ]
-        });
+        try {
+            const { data } = await loginMutation({
+                variables: {
+                    email: values.email,
+                    password: values.password
+                }
+            });
+
+            if (!data) {
+                throw new Error('Couldnt login');
+            }
+
+            auth.login(data.login.token);
+            props.hideModal(false);
+        } catch(e) {
         
+        }
     };
 
     const onFinishFailed = (errorInfo) => {
@@ -55,9 +51,9 @@ const Login = (props) => {
             autoComplete="off"
         >
         <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+            label="Email Address"
+            name="email"
+            rules={[{ required: true, message: 'Please input your Email!' }]}
         >
             <Input />
         </Form.Item>
@@ -68,10 +64,6 @@ const Login = (props) => {
             rules={[{ required: true, message: 'Please input your password!' }]}
         >
             <Input.Password />
-        </Form.Item>
-
-        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-            <Checkbox>Remember me</Checkbox>
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
