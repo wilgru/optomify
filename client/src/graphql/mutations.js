258978b4617a411d96d8553d08cc4affcf8a208c
wilgru <william.gruszka@gmail.com>
2022-07-29 00:23:17 +1000

    added get all patients gql query

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,3 +1,5 @@
+import { gql } from '@apollo/client';
+
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password ) {
@@ -8,3 +10,61 @@ export const LOGIN_USER = gql`
         }
     }
 `
+
+export const CREATE_NEW_PATIENT = gql`
+    mutation CreateNewPatient($firstName: String!, $lastName: String!, $dob: String!, $mobileNumber: String, $email: String!, $hasMedicare: Boolean!, $medicareRef: String, $medicareExp: String) {
+  createNewPatient(first_name: $firstName, last_name: $lastName, dob: $dob, mobile_number: $mobileNumber, email: $email, has_medicare: $hasMedicare, medicare_ref: $medicareRef, medicare_exp: $medicareExp) {
+    _id
+    first_name
+    last_name
+    date_created
+    created_by {
+      _id
+      username
+    }
+  }
+}
+`
+
+export const CREATE_NEW_BOOKING = gql`
+   mutation CreateNewBooking($bookingDate: String!, $bookingStart: String!, $bookingEnd: String!, $onPatientId: ID!, $bookingType: String!) {
+  createNewBooking(booking_date: $bookingDate, booking_start: $bookingStart, booking_end: $bookingEnd, on_patient_id: $onPatientId, booking_type: $bookingType) {
+    _id
+    first_name
+    last_name
+    bookings {
+      _id
+      booking_date
+    }
+  }
+}
+`
+
+export const CREATE_NEW_PATIENT_AND_BOOKING = gql`
+    mutation CreateNewPatientAndBooking($firstName: String!, $lastName: String!, $dob: String!, $email: String!, $hasMedicare: Boolean!, $bookingDate: String!, $bookingStart: String!, $bookingEnd: String!, $bookingType: String!) {
+    createNewPatientAndBooking(first_name: $firstName, last_name: $lastName, dob: $dob, email: $email, has_medicare: $hasMedicare, booking_date: $bookingDate, booking_start: $bookingStart, booking_end: $bookingEnd, booking_type: $bookingType) {
+        _id
+        date
+        open_time
+        closing_time
+        optom_break_start
+        optom_break_end
+        bookings {
+        _id
+        booking_date
+        booking_start
+        booking_end
+        patient {
+            _id
+            first_name
+            last_name
+            dob
+            mobile_number
+            email
+        }
+        booking_note
+        booking_type
+        date_created
+        }
+    }
+}`
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,10 +1,25 @@
 import { gql } from '@apollo/client';
 
+export const GET_ALL_PATIENTS = gql`
+query GetAllPatients {
+  getAllPatients {
+    _id
+    first_name
+    last_name
+    mobile_number
+    email
+  }
+}`
+
 export const GET_BOOK_SETUPS = gql`
     query GetBookSetups($startDate: String!, $endDate: String!) {
     getBookSetups(start_date: $startDate, end_date: $endDate) {
         _id
         date
+        open_time
+        closing_time
+        optom_break_start
+        optom_break_end
         bookings {
             _id
             booking_start
