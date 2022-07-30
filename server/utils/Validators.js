8e5a0e55c2671fcd397c9909e6b64aa447cc5bd0
wilgru <william.gruszka@gmail.com>
2022-07-30 21:13:25 +1000

    updated list of text to match

--- server/utils/Validators.js
+++ server/utils/Validators.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     validateClinicalFileType: {
-        validator: function (value) { return ["Prescription", "Clinical note", "Report"].includes(value) },
+        validator: function (value) { return ["prescription", "clinical note", "report"].includes(value) },
         message: props => `${props.value} File type invalid`
     },
     validateBookingStatus: {
