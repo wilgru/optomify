7a7d1ad0692ea98ef0212c78dcbc1770e642ef80
wilgru <william.gruszka@gmail.com>
2022-08-01 09:55:25 +1000

    fixed bug - dates all referencing the same date causing unexpected mutations

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,11 +1,16 @@
 // https://stackoverflow.com/questions/5210376/how-to-get-first-and-last-day-of-the-current-week-in-javascript
 export const getWeek = (date) => {
-    var curr = new Date; // get current date
-    var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-    var last = first + 6; // last day is the first day + 6
+    const curr = new Date(); // get current date
+    const first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+    const last = first + 6; // last day is the first day + 6
     
-    var firstDay = new Date(curr.setDate(first));
-    var lastDay = new Date(curr.setDate(last));
+    const firstCurr = new Date(curr) // create a copy otherwise itll mess uo the original
+    const firstDay = new Date(firstCurr.setDate(first));
+    console.log(firstDay)
+
+    const lastCurr = new Date(curr) // create a copy otherwise itll mess uo the original
+    const lastDay = new Date(lastCurr.setDate(last));
+    console.log(lastDay)
 
     return {
         firstDay,
