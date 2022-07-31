caf125c71bbe302ea01b71b083500cb13f360ad2
wilgru <william.gruszka@gmail.com>
2022-07-31 20:09:25 +1000

    added useMutation for updating patients details

--- client/src/components/PatientOverview.js
+++ client/src/components/PatientOverview.js
@@ -1,196 +1,222 @@
-import { Button, Checkbox, Form, Input, DatePicker } from 'antd';
+import { Button, Checkbox, Form, Input, DatePicker, Alert } from 'antd';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 
 // grpahQL
 // import { UPDATE_PATIENT } from '../graphql/mutations';
 import { GET_PATIENT } from '../graphql/queries';
+import { UPDATE_PATIENT } from '../graphql/mutations';
 import { useMutation } from '@apollo/client';
 
 const PatientOverview = (props) => {
     const [form] = Form.useForm();
 
-    // const [hasMedicare, setHasMedicare] = useState(true);
-    // useEffect(() => {
-    //     form.setFieldValue({
-    //         first_name: props.patient.first_name,
-    //         last_name: props.patient.last_name,
-    //         dob: props.patient.dob,
-    //         email: props.patient.email,
-    //         mobile_number: props.patient.mobileNumber,
-    //         has_medicare: props.patient.hasMedicare,
-    //         medicare_number: props.patient.medicareNumber,
-    //         medicare_ref: props.patient.medicareRef,
-    //         medicare_exp: props.patient.medicareExp
-    //     })
-    //     console.log(props.patient)
-    // }, [props.patient])
+    const [eligableForMedicare, setEligableForMedicare] = useState(props.patient.has_medicare)
+
+    const [updatePatient, { data, loading, error }] = useMutation(UPDATE_PATIENT);
 
     const onFinish = (values) => {
         console.log('Success:', values);
 
-        // const [updatePatient, { data, loading, error }] = useMutation(UPDATE_PATIENT);
-
-        // updatePatient({
-        //     variables: {
-        //         firstName: values.first_name,
-        //         lastName: values.last_name,
-        //         dob: values.dob,
-        //         email: values.email,
-        //         mobileNumber: values.mobile_number,
-        //         hasMedicare: values.has_medicare,
-        //         medicareNumber: values.medicare_number,
-        //         medicare_Ref: values.medicare_ref,
-        //         medicare_Exp: values.medicare_exp,
-        //         bookingDate: props.patient.bookingDate,
-        //         bookingStart: props.patient.bookingStart,
-        //         bookingEnd: props.patient.bookingEnd,
-        //         bookingType: "General eye test"
-        //     },
-        //     refetchQueries: [
-        //         {query: GET_PATIENT},
-        //         GET_PATIENT
-        //     ]
-        // });
-        
+        updatePatient({
+            variables: {
+                patientToUpdateId: props.patient._id,
+                firstName: values.first_name || props.patient.first_name,
+                lastName: values.last_name || props.patient.last_name,
+                dob: values.dob || props.patient.dob,
+                email: values.email || props.patient.email,
+                mobileNumber: values.mobile_number || props.patient.mobile_number,
+                hasMedicare: values.has_medicare || false,
+                medicareNumber: values.medicare_number || props.patient.medicare_number,
+                medicareRef: values.medicare_ref || props.patient.medicare_ref,
+                medicareExp: values.medicare_exp || props.patient.medicare_exp
+            },
+            refetchQueries: [
+                {query: GET_PATIENT},
+                GET_PATIENT
+            ]
+        });
     };
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
+    //alert
+    const onClose = (e) => {
+        console.log(e, 'I was closed.');
+    };
+
     return (
-    <Form
-        name="basic"
-        labelCol={{
-            span: 5,
-        }}
-        wrapperCol={{
-            span: 16,
-        }}
-        initialValues={{
-            first_name: props.patient.first_name,
-            last_name: props.patient.last_name,
-            dob: moment(new Date(parseInt(props.patient.dob))),
-            email: props.patient.email,
-            mobile_number: props.patient.mobile_number,
-            has_medicare: props.patient.has_medicare,
-            medicare_number: props.patient.medicare_number,
-            medicare_ref: props.patient.medicare_ref,
-            medicare_exp: moment(new Date(parseInt(props.patient.medicare_exp)))
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item
-            label="First Name"
-            name="first_name"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your first name!',
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Last Name"
-            name="last_name"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your last name!',
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="date of birth"
-            name="dob"
-            rules={[
-            {
-                required: true,
-                message: '',
-            },
-            ]}
-        >
-            <DatePicker />
-        </Form.Item>
-
-        <Form.Item
-            label="email"
-            name="email"
-            rules={[
-            {
-                required: true,
-                message: '',
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="mobile_number"
-            name="mobile_number"
-            rules={[
-            {
-                required: true,
-                message: '',
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            name="has_medicare"
-            valuePropName="checked"
-            wrapperCol={{
-            offset: 8,
-            span: 16,
+    <>
+        {error ? (
+            <Alert
+                message="Somethings wrong!"
+                description={error.message}
+                type="error"
+                closable
+                onClose={onClose}
+                style={{
+                    marginBottom:"10px"
+                }}
+            />
+        ) : (
+            <></>
+        )}
+        <Form
+            name="basic"
+            labelCol={{
+                span: 5,
             }}
-        >
-            <Checkbox>Eligable for Medicare</Checkbox>
-        </Form.Item>
-
-        <Form.Item
-            label="medicare number"
-            name="medicare_number"
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="reference number"
-            name="medicare_ref"
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="expiry date"
-            name="medicare_exp"
-        >
-            <DatePicker />
-        </Form.Item>
-
-        <Form.Item
             wrapperCol={{
-            offset: 5,
-            span: 16,
+                span: 16,
             }}
+            initialValues={{
+                first_name: props.patient.first_name,
+                last_name: props.patient.last_name,
+                dob: moment(new Date(parseInt(props.patient.dob))),
+                email: props.patient.email,
+                mobile_number: props.patient.mobile_number,
+                has_medicare: props.patient.has_medicare,
+                medicare_number: props.patient.medicare_number,
+                medicare_ref: props.patient.medicare_ref,
+                medicare_exp: moment(new Date(parseInt(props.patient.medicare_exp)))
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
         >
-            <Button type="primary" htmlType="submit">
-                Update patient details
-            </Button>
-        </Form.Item>
-    </Form>
+            <Form.Item
+                label="First Name"
+                name="first_name"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your first name!',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Last Name"
+                name="last_name"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your last name!',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="date of birth"
+                name="dob"
+                rules={[
+                {
+                    required: true,
+                    message: '',
+                },
+                ]}
+            >
+                <DatePicker />
+            </Form.Item>
+
+            <Form.Item
+                label="email"
+                name="email"
+                rules={[
+                {
+                    required: true,
+                    message: '',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="mobile number"
+                name="mobile_number"
+                rules={[
+                {
+                    required: true,
+                    message: '',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                name="has_medicare"
+                valuePropName="checked"
+                wrapperCol={{
+                offset: 5,
+                span: 16,
+                }}
+            >
+                <Checkbox             
+                onChange={(value) => {
+                    value.target.checked === true
+                    ? setEligableForMedicare(true)
+                    : setEligableForMedicare(false);
+                }}>
+                    Eligable for Medicare
+                </Checkbox>
+            </Form.Item>
+
+            <Form.Item
+                label="medicare number"
+                name="medicare_number"
+                rules={[
+                {
+                    required: eligableForMedicare
+                }
+                ]}
+            >
+                <Input disabled={!eligableForMedicare} />
+            </Form.Item>
+
+            <Form.Item
+                label="reference number"
+                name="medicare_ref"
+                rules={[
+                {
+                    required: eligableForMedicare
+                }
+                ]}
+            >
+                <Input disabled={!eligableForMedicare} />
+            </Form.Item>
+
+            <Form.Item
+                label="expiry date"
+                name="medicare_exp"
+                rules={[
+                {
+                    required: eligableForMedicare
+                }
+                ]}
+            >
+                <DatePicker disabled={!eligableForMedicare} />
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                offset: 5,
+                span: 16,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                    Update patient details
+                </Button>
+            </Form.Item>
+        </Form>
+    </>
     );
 };
 
