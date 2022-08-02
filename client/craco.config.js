235528ad6f4c801a03894de9897241aafdcda1ad
wilgru <william.gruszka@gmail.com>
2022-08-02 23:09:51 +1000

    using craco-antd instead of craco-less

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,23 +1,42 @@
-const CracoLessPlugin = require('craco-less');
+// const CracoLessPlugin = require('craco-less');
+
+// module.exports = {
+//   plugins: [
+//     {
+//       plugin: CracoLessPlugin,
+//       options: {
+//         lessLoaderOptions: {
+//           lessOptions: {
+//             modifyVars: { 
+//                 '@layout-header-background': '#FAFCFC',
+//                 '@primary-color': '#00ACB1',
+//                 '@success-color': '#CAF0C1',
+//                 '@error-color': 'salmon',
+//                 '@text-color': '#015D67',
+//                 // '@menu-item-height': 'fit-content',
+//                 // '@menu-item-padding': 'none'
+//             },
+//             javascriptEnabled: true,
+//           },
+//         },
+//       },
+//     },
+//   ],
+// };
+
+const CracoAntDesignPlugin = require("craco-antd");
 
 module.exports = {
   plugins: [
     {
-      plugin: CracoLessPlugin,
+      plugin: CracoAntDesignPlugin,
       options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: { 
-                '@layout-header-background': '#FAFCFC',
-                '@primary-color': '#00ACB1',
-                '@success-color': '#CAF0C1',
-                '@error-color': 'salmon',
-                '@text-color': '#015D67',
-                // '@menu-item-height': 'fit-content',
-                // '@menu-item-padding': 'none'
-            },
-            javascriptEnabled: true,
-          },
+        customizeTheme: {
+          '@layout-header-background': '#FAFCFC',
+          '@primary-color': '#00ACB1',
+          '@success-color': '#CAF0C1',
+          '@error-color': 'salmon',
+          '@text-color': '#015D67'
         },
       },
     },
