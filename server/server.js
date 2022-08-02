f3aa4ae21123198272b98ae1b44923b316d5b1d0
wilgru <william.gruszka@gmail.com>
2022-08-02 18:29:14 +1000

    removed unused code

--- server/server.js
+++ server/server.js
@@ -29,6 +29,20 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+// console.log('\n\n')
+// console.log(process.env.TINYMCE_API_KEY)
+// console.log('\n\n')
+
+// app.get('/api/tinymce', (req, res) => {
+//   console.log("req.headers.authorization")
+//   console.log(req.headers.authorization)
+//   if (authMiddleware({req}).user) {
+//     res.json({
+//       tinymce_api_key: process.env.TINYMCE_API_KEY
+//     })
+//   }
+// })
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
