d26e6a847594deb51be80bfa00b42227a9d2a9ca
wilgru <william.gruszka@gmail.com>
2022-08-04 08:45:48 +1000

    import serviceworkerregistration

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,27 +17,7 @@ root.render(
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-
-// import React, { useState } from 'react';
-// import { render } from 'react-dom';
-// import { DatePicker, message } from 'antd';
-// import 'antd/dist/antd.css';
-// import './index.css';
-
-// const App = () => {
-//   const [date, setDate] = useState(null);
-//   const handleChange = value => {
-//     message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-//     setDate(value);
-//   };
-//   return (
-//     <div style={{ width: 400, margin: '100px auto' }}>
-//       <DatePicker onChange={handleChange} />
-//       <div style={{ marginTop: 16 }}>
-//         Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-//       </div>
-//     </div>
-//   );
-// };
-
-// render(<App />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
