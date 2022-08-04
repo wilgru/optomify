7061e0b8551c0eb281fe221d452f22685e2afb8c
wilgru <william.gruszka@gmail.com>
2022-08-04 11:44:54 +1000

    moved date functions defined elsewhere to utils

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 // https://stackoverflow.com/questions/5210376/how-to-get-first-and-last-day-of-the-current-week-in-javascript
 export const getWeek = (date) => {
     const curr = new Date(); // get current date
@@ -20,16 +22,33 @@ export const getWeek = (date) => {
     }
 }
 
+export const getTimeRelation = (nowTime, checkingTime) => {
+    // console.log(nowTime, checkingTime)
+    if (moment(checkingTime).isBefore(nowTime) && moment(nowTime).isBefore(moment(checkingTime).add(30, 'm'))) {
+        return "current"
+    } else if (moment(checkingTime).isBefore(nowTime)) {
+        return "past"
+    } else {
+        return "future"
+    }
+}
+
 // date working
 export const dateWorker = (date) => {
-    let b = String(date)
-    let c = b.split(" ", 5)
-    let d = c.join(" ")
-    let e = d+" UTC" 
-    let f = new Date(e)
-    let g = f.toISOString()
-    let h = g.split(".")[0]
-    let i = h+"+00.00"
-
-    return i
+    // should get a date string, and is essentially just adding 10hrs. will return same type of string just 10 hours in the future.
+    return moment(date).add(10, 'h').toDate()
 }
+
+// date working
+// export const dateWorker = (date) => {
+//     let b = String(date)
+//     let c = b.split(" ", 5)
+//     let d = c.join(" ")
+//     let e = d+" UTC" 
+//     let f = new Date(e)
+//     let g = f.toISOString()
+//     let h = g.split(".")[0]
+//     let i = h+"+00.00"
+
+//     return i
+// }
